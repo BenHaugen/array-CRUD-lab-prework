8eb12a0893803da229ff5ee88a9c5fb98b3a7a81
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-23 02:14:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
- create_an_array = ["ben", "john", "alex", "bob"]
+ ["ben", "john", "alex", "bob"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
